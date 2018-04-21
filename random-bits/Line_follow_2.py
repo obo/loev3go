572a7c308dae68c5fc2853020895ac03d317f429
Ondrej Bojar <bojar@ufal.mff.cuni.cz>
2018-04-21 23:05:21 +0000

    rgb-sum-line-follower

---
+++ random-bits/Line_follow_2.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+# from https://raw.githubusercontent.com/Klabbedi/ev3/master/Line_follow_2.py
+# coding: utf-8
+
+from time import sleep
+
+from ev3dev.auto import *
+
+# ------Input--------
+print('Setting input values')
+power = 60
+power = 20
+power = 60
+target = 55
+kp = float(0.65) # Proportional gain. Start value 1
+kp = float(0.95) # Proportional gain. Start value 1
+kd = 1           # Derivative gain. Start value 0
+ki = float(0.02) # Integral gain. Start value 0
+direction = -1
+  # set to -1 for dark on the right hand side
+# identify the min and max with report-color-values.py
+minRef = 290 # sub of RGB for the dark *right* side of the line
+maxRef = 500 # sub of RGB for the light *left* side of the line
+# -------------------
+
+# Connect two large motors on output ports B and C and check that
+# the device is connected using the 'connected' property.
+print('Connecting motors')
+left_motor = LargeMotor(OUTPUT_B);  assert left_motor.connected
+right_motor = LargeMotor(OUTPUT_C); assert right_motor.connected
+# One left and one right motor should be connected
+
+# Connect color and touch sensors and check that they
+# are connected.
+print('Connecting sensors')
+#ir = InfraredSensor();         assert ir.connected
+ts = TouchSensor();            assert ts.connected
+col= ColorSensor();     assert col.connected
+
+# Change color sensor mode
+print('Setting color sensor mode')
+#col.mode = 'COL-REFLECT'
+col.mode = 'RGB-RAW' # use rgb sum
+
+# Adding button so it would be possible to break the loop using
+# one of the buttons on the brick
+print('Adding button')
+btn = Button()
+
+def steering2(course, power):
+        """
+        Computes how fast each motor in a pair should turn to achieve the
+        specified steering.
+        Input:
+                course [-100, 100]:
+                * -100 means turn left as fast as possible,
+                *  0   means drive in a straight line, and
+                *  100  means turn right as fast as possible.
+                * If >100 power_right = -power
+                * If <100 power_left = power        
+        power: the power that should be applied to the outmost motor (the one
+                rotating faster). The power of the other motor will be computed
+                automatically.
+        Output:
+                a tuple of power values for a pair of motors.
+        Example:
+                for (motor, power) in zip((left_motor, right_motor), steering(50, 90)):
+                        motor.run_forever(speed_sp=power)
+        """
+        if course >= 0:
+                if course > 100:
+                        power_right = 0
+                        power_left = power
+                else:   
+                        power_left = power
+                        power_right = power - ((power * course) / 100)
+        else:
+                if course < -100:
+                        power_left = 0
+                        power_right = power
+                else:
+                        power_right = power
+                        power_left = power + ((power * course) / 100)
+        return (int(power_left), int(power_right))
+
+def run(power, target, kp, kd, ki, direction, minRef, maxRef):
+        """
+        PID controlled line follower algoritm used to calculate left and right motor power.
+        Input:
+                power. Max motor power on any of the motors
+                target. Normalized target value.
+                kp. Proportional gain
+                ki. Integral gain
+                kd. Derivative gain
+                direction. 1 or -1 depending on the direction the robot should steer
+                minRef. Min reflecting value of floor or line
+                maxRef. Max reflecting value of floor or line 
+        """
+        lastError = error = integral = 0
+        left_motor.run_direct()
+        right_motor.run_direct()
+        while not btn.any() :
+                if ts.value():
+                       print('Breaking loop')# User pressed touch sensor
+                       break
+                #refRead = col.value()
+                refRead = col.value(0)+col.value(1)+col.value(2)
+                error = target - (100 * ( refRead - minRef ) / ( maxRef - minRef ))
+                derivative = error - lastError
+                lastError = error
+                integral = float(0.5) * integral + error
+                course = (kp * error + kd * derivative +ki * integral) * direction
+                for (motor, pow) in zip((left_motor, right_motor), steering2(course, power)):
+                        motor.duty_cycle_sp = pow
+                sleep(0.01) # Aprox 100Hz
+
+run(power, target, kp, kd, ki, direction, minRef, maxRef)
+
+# Stop the motors before exiting.
+print('Stopping motors')
+left_motor.stop()
+right_motor.stop()
--- random-bits/report-color-values.py
+++ random-bits/report-color-values.py
@@ -3,11 +3,12 @@ import time
 
 cl = ev3.ColorSensor()
 cl.mode='RGB-RAW'
-print("Calibrating, hopefully looking at something white now.")
-cl.calibrate_white()
+#print("Calibrating, hopefully looking at something white now.")
+#cl.calibrate_white()
+## does not seem to be in the released version yet
 while True:
     red = cl.value(0)
     green=cl.value(1)
     blue=cl.value(2)
-    print("Red: " + str(red) + ", Green: " + str(green) + ", Blue: " + str(blue))
+    print("Sum: "+str(red+green+blue)+", Red: " + str(red) + ", Green: " + str(green) + ", Blue: " + str(blue))
     time.sleep(1)
